39895313c0dba29e9584a0e3828a7023d4c3a71f
gitstar123 <92081296+gitstar123@users.noreply.github.com>
2022-01-23 00:42:24 +0530

    Add files via upload
    Addition of features and fixing.

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import { PropTypes } from 'react'
 export default function TextForm(props) {
 
         const [text, setText] = useState("Original Text");
@@ -16,6 +15,14 @@ export default function TextForm(props) {
         function onchange(e){
           setText(e.target.value);
         }
+        function clear(){
+          setText('');
+          props.makeAlert('cleared','success');
+        }
+        function copy(){
+          navigator.clipboard.writeText(text);
+          props.makeAlert('copied to clipboard','success');
+        }
         const mystyles = {
           color : props.mode === 'light'? 'black' : 'white'
         };
@@ -30,14 +37,18 @@ export default function TextForm(props) {
                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value = {text} onChange = {onchange} style = {areaStyle}></textarea>
             </div>
             <div className="container">
-                <button className='btn btn-light' onClick = {handle}>Convert to Uppercase</button>
+                <button className='mx-2 btn btn-dark' onClick = {handle}>Convert to Uppercase</button>
                 <button className='mx-2 btn btn-dark' onClick = {handlelow}>Convert to Lowercase</button>
+                <button className='mx-2 btn btn-dark' onClick = {clear}>Clear Text</button>
+                <button className='mx-2 btn btn-dark' onClick = {copy}>Copy Text</button>
                 </div>
                 <div className="container" style={mystyles}>
                 <h1>Word Count</h1>
-                <p>{text.split(" ").length} Words and {text.length} Characters</p>
+                <p>{text.split(/\s+/).filter((e)=>{return e.length!==0}).length} Words and {text.length} Characters</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text === "" ? 'Enter something to preview' : text}</p>
+                <h2>Read Time</h2>
+                <p>{text.split(" ").filter((e)=>{return e.length!==0}).length*0.008} minutes</p>
             </div>
         </div>
     )
